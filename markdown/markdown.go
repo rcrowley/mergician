c43230729c1b937a402e816cd5e27f05f8741630
Richard Crowley <r@rcrowley.org>
2024-01-10 01:50:20 -0800

    I trust myself to put HTML in Markdown

--- markdown/markdown.go
+++ markdown/markdown.go
@@ -3,10 +3,13 @@ package markdown
 import (
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/renderer/html"
 )
 
 func markdown() goldmark.Markdown {
 	return goldmark.New(
-		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithExtensions(extension.Typographer),
+		goldmark.WithRendererOptions(html.WithUnsafe()),
+		// TODO <https://github.com/yuin/goldmark-highlighting>
 	)
 }
