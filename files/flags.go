2d003129eaec6e6cafadb8f016180cbad4be8b92
Richard Crowley <r@rcrowley.org>
2024-12-27 06:09:39 -0800

    StringSliceFlag constructor that takes a flagSet

--- files/flags.go
+++ files/flags.go
@@ -1,8 +1,17 @@
 package files
 
-import "strings"
+import (
+	"flag"
+	"strings"
+)
 
 type StringSliceFlag []string
+
+func NewStringSliceFlag(flagSet *flag.FlagSet, name, usage string) *StringSliceFlag {
+	f := &StringSliceFlag{}
+	flagSet.Var(f, name, usage)
+	return f
+}
 
 func (f *StringSliceFlag) String() string {
 	return strings.Join(*f, ", ")
