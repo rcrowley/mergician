0e59e812cd09d9f4bd3982f448d75fe8befa4f3f
Richard Crowley <r@rcrowley.org>
2025-01-29 21:38:42 -0800

    Factor out another couple of test files

--- html/merge_test.go
+++ html/merge_test.go
@@ -24,23 +24,7 @@ func TestMergeCustom(t *testing.T) {
 	}
 
 	actual := String(n)
-	expected := `<!DOCTYPE html>
-<html lang="en">
-<head>
-<link href="template.css" rel="stylesheet"/>
-<meta charset="utf-8"/>
-<meta content="width=device-width,initial-scale=1" name="viewport"/>
-<title>My cool webpage — Website</title>
-</head>
-<body>
-<header><h1>Website</h1></header>
-<br/><!-- explicit self-closing -->
-<article class="body">Things</article>
-<br/><!-- implied self-closing -->
-<footer><p>© 2023</p></footer>
-</body>
-</html>
-`
+	expected := testTemplatePlusArticleCustomHTML(t)
 	if actual != expected {
 		t.Fatalf("actual: %s != expected: %s", actual, expected)
 	}
@@ -48,7 +32,7 @@ func TestMergeCustom(t *testing.T) {
 	//t.Log(String(n))
 }
 
-func TestMergeCustomEmpty(t *testing.T) {
+func TestMergeNoRules(t *testing.T) {
 	n0, err := ParseFile("testdata/template.html")
 	if err != nil {
 		t.Fatal(err)
@@ -63,25 +47,7 @@ func TestMergeCustomEmpty(t *testing.T) {
 	}
 
 	actual := String(n)
-	expected := `<!DOCTYPE html>
-<html lang="en">
-<head>
-<link href="template.css" rel="stylesheet"/>
-<meta charset="utf-8"/>
-<meta content="width=device-width,initial-scale=1" name="viewport"/>
-<title>My cool webpage — Website</title>
-</head>
-<body>
-<header><h1>Website</h1></header>
-<br/><!-- explicit self-closing -->
-<article class="body">
-<p>Overwritten.</p>
-</article>
-<br/><!-- implied self-closing -->
-<footer><p>© 2023</p></footer>
-</body>
-</html>
-`
+	expected := testTemplatePlusArticleNoRulesHTML(t)
 	if actual != expected {
 		t.Fatalf("actual: %s != expected: %s", actual, expected)
 	}
@@ -112,8 +78,24 @@ func TestMergeDefault(t *testing.T) {
 	//t.Log(String(n))
 }
 
+func testTemplatePlusArticleCustomHTML(t *testing.T) string {
+	b, err := os.ReadFile("testdata/template+article.custom.html")
+	if err != nil {
+		t.Fatal(err)
+	}
+	return string(b)
+}
+
 func testTemplatePlusArticleHTML(t *testing.T) string {
 	b, err := os.ReadFile("testdata/template+article.html")
+	if err != nil {
+		t.Fatal(err)
+	}
+	return string(b)
+}
+
+func testTemplatePlusArticleNoRulesHTML(t *testing.T) string {
+	b, err := os.ReadFile("testdata/template+article.no-rules.html")
 	if err != nil {
 		t.Fatal(err)
 	}
